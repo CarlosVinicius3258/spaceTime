059d8e0f7d618abe8c6adf1b5a4d0c15901e1c57
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-05-17 00:04:59 -0300

    config fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className='h-screen bg-zinc-950 p-6'>
+      <h1 className='font-bold text-white text-5xl'>Hello World</h1>
+      <h1 className='font-bold text-white text-5xl font-alt'>Hello World</h1>
+    </div>
+
   );
 }
