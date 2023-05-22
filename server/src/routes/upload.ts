10be25747d99e8cc01d4e08a01188a2755edb3b8
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-05-22 00:41:11 -0300

    add(server): add file(image/video) upload on server

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,42 @@
+import { randomUUID } from 'crypto';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { createWriteStream } from 'fs';
+import { extname, resolve } from 'path';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+const pump = promisify(pipeline);
+
+const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/;
+export async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (request, reply) => {
+    const upload = await request.file({
+      limits: {
+        fileSize: 5_242_880, // 5MB
+      },
+    });
+
+    if (!upload) {
+      return reply.status(400).send();
+    }
+
+    const isValidFileOFormat = mimeTypeRegex.test(upload.mimetype);
+
+    if (!isValidFileOFormat) {
+      return reply.status(400).send();
+    }
+
+    const fileId = randomUUID();
+    const extension = extname(upload.filename);
+
+    const fileName = fileId.concat(extension);
+
+    const writeStream = createWriteStream(resolve(__dirname, '../../uploads/', fileName));
+
+    await pump(upload.file, writeStream);
+
+    const fullURl = request.protocol.concat('://').concat(request.hostname);
+    const fileURL = new URL(`/uploads/${fileName}`, fullURl).toString();
+    return { fileURL };
+  });
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,30 @@ import cors from '@fastify/cors';
 import 'dotenv/config';
 import { authRoutes } from './routes/auth';
 import fastifyJwt from '@fastify/jwt';
+import fastifyMultipart from '@fastify/multipart';
+import { uploadRoutes } from './routes/upload';
+import { fastifyStatic } from '@fastify/static';
+import { resolve } from 'path';
 const app = fastify();
 
-app.register(memoriesRoutes);
-app.register(authRoutes);
+//Middlewares/Libs
+app.register(fastifyMultipart);
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+});
 app.register(fastifyJwt, {
   secret: `${process.env.JWT_SECRET}`,
 });
 app.register(cors, {
   origin: true,
 });
+
+//Routes
+app.register(memoriesRoutes);
+app.register(authRoutes);
+app.register(uploadRoutes);
+
 app
   .listen({
     port: 3333,
