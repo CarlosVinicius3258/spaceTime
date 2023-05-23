b61388c557cc089a7fabb432688cdfd49fb9f7c5
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-05-22 23:06:18 -0300

    add(web): add MediaPicker and image preview on add memory

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
+
+interface MediaPickerProps {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null);
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target;
+
+    if (!files) {
+      return;
+    }
+
+    const previewURL = URL.createObjectURL(files[0]);
+    setPreview(previewURL);
+  }
+
+
+  return (
+    <>
+      <input onChange={ onFileSelected } type="file" id='media' accept='image/*' className='invisible h-0 w-0' />
+      { preview && <img src={ preview } alt='' className='w-full  rounded-lg object-contain' /> }
+    </>
+  );
+
+}
